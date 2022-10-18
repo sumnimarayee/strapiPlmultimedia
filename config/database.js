888a0151b8a95e1aa524803aba30eb12b71940c1
Sunima Rai <np01cp4s210154@islingtoncollege.edu.np>
2022-10-18 20:47:01 +0545

    first commit

---
+++ config/database.js
@@ -0,0 +1,26 @@
+  // strapi-api/config/database.js
+  module.exports = ({ env }) => ({
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3000),
+        database: env('DATABASE_NAME', 'database'),
+        user: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'password123@$#'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      }
+  }
+});
+
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });

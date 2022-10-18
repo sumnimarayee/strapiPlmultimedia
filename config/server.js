3c5843d10a4132cd971ea97f261fb7d839d38efe
Sunima Rai <np01cp4s210154@islingtoncollege.edu.np>
2022-10-18 21:58:43 +0545

    updated the gitignore

--- config/server.js
+++ config/server.js
@@ -1,11 +1,11 @@
-// module.exports = ({ env }) => ({
-//   host: env("HOST", "0.0.0.0"),
-//   port: env.int("PORT", 1338),
-//   app: {
-//     keys: env.array("APP_KEYS"),
-//   },
-// });
-
 module.exports = ({ env }) => ({
-  url: env('MY_HEROKU_URL'),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1338),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
 });
+
+// module.exports = ({ env }) => ({
+//   url: env('MY_HEROKU_URL'),
+// });
